3d41cead3846bca1afe35a8a4dfb6ab55b510363
FranziskaTaubert <89009739+FranziskaTaubert@users.noreply.github.com>
2024-07-16 17:15:19 +0200

    Add files via upload

---
+++ src/utils/utils.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <direct.h>
+#include <vector>
+#include <string>
+
+class UTILS
+{
+public:
+    UTILS();
+    ~UTILS();
+
+    std::vector<std::string> strings;
+    void splitString(std::string str, char separator);
+    bool stringToBool(const std::string &str);
+    void handleError(std::string errorString);
+    std::string getFileEnding(std::string file);
+    int calculateJulianDayFromDate(int day, int month, int year);
+};
