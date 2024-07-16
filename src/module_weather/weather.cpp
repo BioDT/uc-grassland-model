99d49c88a64e9f2d33b40fb1cb1629cf46564538
FranziskaTaubert <89009739+FranziskaTaubert@users.noreply.github.com>
2024-07-16 17:16:54 +0200

    Add files via upload

---
+++ src/module_weather/weather.cpp
@@ -0,0 +1,16 @@
+#include "weather.h"
+
+WEATHER::WEATHER(){};
+WEATHER::~WEATHER(){};
+
+int WEATHER::calculateAstronomicDayLength(void)
+{
+    // TODO: add calculation with Thomas
+    return 12;
+}
+
+double WEATHER::calculatePotentialEvapoTranspiration(void)
+{
+    // TODO: add calculation with Thomas
+    return 3.5;
+}
---
+++ src/module_weather/weather.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+class WEATHER
+{
+public:
+    WEATHER();
+    ~WEATHER();
+
+    // weather input variables
+    std::vector<std::string> weatherDates;
+    std::vector<double> precipitation;
+    std::vector<double> airTemperature;
+    std::vector<double> photosyntheticPhotonFluxDensity;
+    std::vector<double> potEvapoTranspiration;
+    std::vector<double> dayLength;
+
+    int calculateAstronomicDayLength(void);
+    double calculatePotentialEvapoTranspiration(void);
+};
